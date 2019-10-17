e4d1ed164bcb0be38876696d70de16deb3a17e18
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2019-10-17 22:33:01 +0300

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { BeneficiariesComponent } from './components/beneficiaries/beneficiaries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    BeneficiariesComponent
   ],
   imports: [
     BrowserModule
