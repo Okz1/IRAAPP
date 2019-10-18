c46601fd0a03d35a4a359e9dd5602f3041a1b546
Oleg Mykhalyna <oleg_mykhalyna@epam.com>
2019-10-18 13:45:42 +0300

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { BeneficiariesComponent } from './components/beneficiaries/beneficiaries.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SideMenuComponent,
-    BeneficiariesComponent
+    BeneficiariesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/beneficiaries/beneficiaries.component.ts
+++ src/app/components/beneficiaries/beneficiaries.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+interface Beneficiar {
+  name: string;
+  date: string | Date;
+  type: string;
+  optional: string;
+  relationShip: boolean;
+  amount: number;
+}
 
 @Component({
   selector: 'app-beneficiaries',
@@ -7,9 +17,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeneficiariesComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  get beneficiariesArray(): FormArray {
+    return this.form.controls['beneficiaries'] as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  addBeneficiar(): void {
+    this.beneficiariesArray.push(this.creteBeneficiar());
+  }
+
+  removeBeneficiar(beneficiarIndex: number): void {
+    if (!(this.beneficiariesArray.value.length === 1)) {
+      this.beneficiariesArray.removeAt(beneficiarIndex);
+    }
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      beneficiaries: this.fb.array([this.creteBeneficiar()])
+    });
+  }
+
+  private creteBeneficiar(): FormGroup {
+    return this.fb.group({
+      name: [''],
+      date: [''],
+      type: [''],
+      optional: [''],
+      relationShip: [''],
+      amount: [''],
+    });
   }
 
 }
